6a131f9a7c3cca5b61395feaa5be63b41b283ad7
visheshjeswani33 <64632774+visheshjeswani33@users.noreply.github.com>
2020-11-29 16:27:57 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,106 @@
+class Game {
+  constructor(){
+
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+
+    car1 = createSprite(250,200);
+    car1.addImage(c1img);
+    car2 = createSprite(550,200);
+    car2.addImage(c2img);
+    car3 = createSprite(750,200);
+    car3.addImage(c3img);
+    car4 = createSprite(950,200);
+    car4.addImage(c4img);
+    cars = [car1, car2, car3, car4];
+  }
+
+  play(){
+    form.hide();
+
+  Player.getPlayerInfo();
+  player.getCarsAtEnd();
+
+  if(allPlayers !== undefined){
+  //var display_position = 100;
+  background("#c68767");
+  image(track,0,-displayHeight*4,displayWidth,displayHeight*5);
+
+  //index of the array
+  var index = 0;
+
+  //x and y position of the cars
+  var x = 180;
+  var y;
+
+  for(var plr in allPlayers){
+  //add 1 to the index for every loop
+  index = index + 1 ;
+
+  //position the cars a little away from each other in x direction
+  x = x + 200;
+  //use data form the database to display the cars in y direction
+  y = displayHeight - allPlayers[plr].distance;
+  cars[index-1].x = x;
+  cars[index-1].y = y;
+
+  if (index === player.index){
+
+    stroke(10);
+    fill("red");
+    ellipse(x,y,60,60);
+    camera.position.x = displayWidth/2;
+    camera.position.y = cars[index-1].y
+}
+  //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+}
+
+}
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=10
+      player.update();
+}
+    if(player.distance>3800){
+
+      gameState = 2;
+      player.rank= player.rank+1;
+      Player.updateCarsAtEnd(player.rank);
+      textSize(25);
+      fill("red")
+      text("Your Rank :"+ player.rank, 350, y-120);
+    }
+    drawSprites();
+  }
+
+  end(){
+
+   console.log("Game Ended");
+
+  }
+}
